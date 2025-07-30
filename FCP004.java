469e91e593ceaed8b9c90c293070f189c7b0a7de
Xcelevate-Dharmaraj <dharmaraj.2533@xcelevate.org>
2025-07-30 10:21:35 +0530

    Add files via upload

---
+++ FCP004.java
@@ -0,0 +1,12 @@
+ import java.util.*;
+public class FCP004{
+    public static void main(String[] args){
+Scanner ab = new Scanner(System.in);
+int b=ab.nextInt();
+int c=ab.nextInt();
+System.out.println(b+c);
+System.out.println(b-c);
+System.out.println(b*c);
+System.out.println((float)((float)b/c));
+System.out.println(b/c);
+System.out.println(b%c);}}
