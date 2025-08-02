277e9290744db0eacaf339decc727062b29d3ef1
Xcelevate-Dharmaraj <dharmaraj.2533@xcelevate.org>
2025-08-02 14:33:46 +0530

    Add files via upload

---
+++ FCP025.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FCP025{
+    public static void main(String[] args){
+Scanner ab = new Scanner(System.in);
+int a = ab.nextInt();
+int c = 0;
+for(int i = 1; i<a; i =i +2 ) {
+c = c+i;
+}
+System.out.println(c);
+
+}
+}
